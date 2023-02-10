e67f34092c7cf5e8619eb9dac0a52534e167782a
dima <zeionara@niuitmo.ru>
2023-02-10 22:13:03 +0000

    Added swift installation instructions

--- setup.sh
+++ setup.sh
@@ -18,3 +18,21 @@ sudo mkdir /usr/local/bin || quit 'Cannot create directory for installing miniku
 sudo install minikube /usr/local/bin || quit 'Cannot install minikube'
 
 rm minikube || quit 'Cannot delete minikube executable'
+
+install_swift () {
+    archive_path=${0:-/tmp/swift.tar.gz}
+
+    wget https://download.swift.org/swift-5.7.3-release/ubuntu2204/swift-5.7.3-RELEASE/swift-5.7.3-RELEASE-ubuntu22.04.tar.gz -O $archive_path || quit 'Cannot download swift'
+    tar -xzvf $archive_path || quit 'Cannot unpack swift installation'
+    rm $archive_path || quit 'Cannot remove downloaded archive'
+
+    swift_root=/usr/local/swift-5.7.3
+    sudo mv swift-5.7.3-RELEASE-ubuntu22.04 $swift_root || quit 'Cannot move unpacked folder'
+    echo -e "\nexport PATH=\$PATH:$swift_root/usr/bin" >> ~/.bashrc || quit 'Cannot update bashrc'
+
+    # if doesn't work, try installing version for ubuntu 18.04 instead of 22.04
+}
+
+# 4. Install swift
+
+swift -v || install_swift || quit 'Cannot install swift'
